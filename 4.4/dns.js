8d48402d2544dc72a94aff0a5053b77a0ebbcf19
Diego Vinci <[omitted]>
2020-04-19 15:28:44 -0300

    4.4: add some important explanations on dns mehords

--- 4.4/dns.js
+++ 4.4/dns.js
@@ -1,17 +1,20 @@
 const dns = require('dns'); // name -- addresses
 
-// dns.lookup('pluralsight.com', (err, address) => {
-//   console.log(address);
-// });
+// The lookup method on the dns module does not perform any network communication, and instead uses the underlying OS facilities to perform name resolutions.
+// This means that it will be using libuv threads.
+dns.lookup('pluralsight.com', (err, address) => {
+  console.log(address);
+});
 
-// dns.resolve4('pluralsight.com', (err, address) => {
-//   console.log(address);
-// });
+// All the other methods uses the network directly and does not use libuv threads.
+dns.resolve4('pluralsight.com', (err, address) => {
+  console.log(address);
+});
 
-// dns.resolveMx('pluralsight.com', (err, address) => {
-//   console.log(address);
-// });
+dns.resolveMx('pluralsight.com', (err, address) => {
+  console.log(address);
+});
 
-// dns.reverse('35.161.75.227', (err, hostnames) => {
-//   console.log(hostnames);
-// });
+dns.reverse('35.161.75.227', (err, hostnames) => {
+  console.log(hostnames);
+});
