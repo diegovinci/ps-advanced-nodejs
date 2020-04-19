bb77cf083300ea97bfe14d53feae6996f933eb0b
Diego Vinci <[omitted]>
2020-04-19 20:11:23 -0300

    6.2: add some additional info about fs module

--- 6.2/index.js
+++ 6.2/index.js
@@ -12,4 +12,6 @@ fs.readFile(__filename, (err, data) => {
 // Synchronous Form:
 const data = fs.readFileSync(__filename);
 // exceptions are immediately thrown
+// if you don't want the errors to bubble up, you need to use a try/catch to handle them
 // do something with data
+// the readFile method return a buffer if a character enconding is not defined
