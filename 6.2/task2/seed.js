3d76d30095cff2decedb7a11227bd2e340ace234
Diego Vinci <[omitted]>
2020-04-19 19:58:55 -0300

    6.2: add notes about task 2

--- 6.2/task2/seed.js
+++ 6.2/task2/seed.js
@@ -3,16 +3,31 @@ const path = require('path');
 const dirname = path.join(__dirname, 'files');
 
 fs.mkdirSync(dirname);
-const ms1Day = 24*60*60*1000;
+const ms1Day = 24 * 60 * 60 * 1000;
 
-for (let i=0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   const filePath = path.join(dirname, `file${i}`);
+  // Creating 10 files to test the script solution
   fs.writeFile(filePath, i, (err) => {
     if (err) throw err;
 
-    const time = (Date.now() - i*ms1Day)/1000;
+    const time = (Date.now() - i * ms1Day) / 1000;
+    // Changing the timestamp. filePath, time: access time, time: modified time
     fs.utimes(filePath, time, time, (err) => {
       if (err) throw err;
     });
   });
 }
+
+// As the result these files is created in files directory
+// file0
+// file1
+// file2
+// file3
+// file4
+// file5
+// file6
+// file7
+// file8
+// file9
+// file10
--- 6.2/task2/solution.js
+++ 6.2/task2/solution.js
@@ -1,16 +1,23 @@
+/*
+TASK 2:
+Script to clean old files in a directory.
+Anything older than 7 days should be deleted.
+ */
+
 const fs = require('fs');
 const path = require('path');
 const dirname = path.join(__dirname, 'files');
 
 const files = fs.readdirSync(dirname);
-const ms1Day = 24*60*60*1000;
+const ms1Day = 24 * 60 * 60 * 1000;
 
 files.forEach(file => {
   const filePath = path.join(dirname, file);
   fs.stat(filePath, (err, stats) => {
     if (err) throw err;
 
-    if ((Date.now() - stats.mtime.getTime() > 7*ms1Day)) {
+    // Get the files that was not modified in the last 7 days - stats.mtime
+    if ((Date.now() - stats.mtime.getTime() > 7 * ms1Day)) {
       fs.unlink(filePath, (err) => {
         if (err) throw err;
         console.log(`deleted ${filePath}`);
@@ -18,3 +25,5 @@ files.forEach(file => {
     }
   });
 });
+
+// As the result file7, file8, file9 are deleted.
