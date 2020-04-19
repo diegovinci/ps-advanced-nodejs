b5ef5149f5e9bc7858eb2c480e04ce217108a73f
Diego Vinci <[omitted]>
2020-04-19 19:58:48 -0300

    6.2: add notes about task 1

--- 6.2/task1/solution.js
+++ 6.2/task1/solution.js
@@ -1,15 +1,26 @@
+/*
+TASK 1:
+Script to fix files in a directory.
+Each file has its data duplicated.
+Truncate each file in half.
+*/
+
 const fs = require('fs');
 const path = require('path');
+
 const dirname = path.join(__dirname, 'files');
+console.log("dirname", dirname); // /home/user-name/projects/project-subfolder/ps-nodejs-advanced/6.2/task1/files
 
 const files = fs.readdirSync(dirname);
+console.log("files", files); // [ 'http.js', 'https.js', 'index.js', 'index2.js', 'net.js' ]
 
 files.forEach(file => {
   const filePath = path.join(dirname, file);
   fs.stat(filePath, (err, stats) => {
-    if (err) throw err;
+    console.log("stats", stats); // { ..., size: 726, ...}
 
-    fs.truncate(filePath, stats.size/2, (err) => {
+    if (err) throw err;
+    fs.truncate(filePath, stats.size / 2, (err) => {
       if (err) throw err;
     });
   });
