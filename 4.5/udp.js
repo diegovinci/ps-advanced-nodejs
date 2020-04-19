502951afc3f2a5f65b03d01488dc195dc34410b9
Diego Vinci <[omitted]>
2020-04-19 16:30:10 -0300

    4.5: add other pieces of code to test dgram features

--- 4.5/udp.js
+++ 4.5/udp.js
@@ -15,12 +15,16 @@ server.bind(PORT, HOST);
 
 // Client
 
-const client = dgram.createSocket('udp4');
-const msg = Buffer.from('Pluralsight rocks');
+// The setInterval wrapper is to show that dgram.createSocket() connects to a diferent port every time
+// setInterval(() => {
+  const client = dgram.createSocket('udp4');
+  const msg = Buffer.from('Pluralsight rocks');
 
-client.send(msg, 0, msg.length, PORT, HOST, (err) => {
-  if (err) throw err;
+  // client.send('Pluralsight rocks', PORT, HOST, (err) => {
+  client.send(msg, 0, msg.length, PORT, HOST, (err) => {
+    if (err) throw err;
 
-  console.log('UDP message sent');
-  client.close();
-});
+    console.log('UDP message sent');
+    client.close();
+  });
+// }, 1000);
