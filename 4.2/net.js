cbee2514658e8b596aba400035270e953d4e3bd5
Diego Vinci <[omitted]>
2020-04-19 14:54:30 -0300

    4.2: add comments to explain the details of the implementation

--- 4.2/net.js
+++ 4.2/net.js
@@ -10,6 +10,7 @@ server.on('connection', socket => {
   socket.write('Welcome new client!\n');
 
   socket.on('data', data => {
+    // On data from any socket, we loop over this socket objects and write the received data to every socket in there.
     Object.entries(sockets).forEach(([, cs]) => {
       cs.write(`${socket.id}: `);
       cs.write(data);
@@ -17,9 +18,11 @@ server.on('connection', socket => {
   });
 
   socket.on('end', () => {
+    // Delete the socket to prevent the application from crashing when a client disconnects from the server.
     delete sockets[socket.id];
     console.log('Client disconnected');
   });
 });
 
 server.listen(8000, () => console.log('Server bound'));
+// For testing purposes, telnet or netcat can be used multiple times. Eg: nc localhost 8000
