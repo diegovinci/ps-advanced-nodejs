290ac3dbf547503f297be07829a973b2c6627e1f
Diego Vinci <[omitted]>
2020-04-19 19:08:52 -0300

    5.5: add a few notes about url module

--- 5.5/url.js
+++ 5.5/url.js
@@ -1,11 +1,10 @@
 const url = require('url');
 
-const urlString =
-  'https://www.pluralsight.com/search?q=buna';
+const urlString = 'https://www.pluralsight.com/search?q=buna';
 
-console.log(
-  url.parse(urlString, true)
-);
+console.log(url.parse(urlString));
+console.log(url.parse(urlString, true)); // parse query string as well
+
 
 const urlObject = {
   protocol: 'https',
@@ -14,6 +13,4 @@ const urlObject = {
   pathname: '/search',
 };
 
-console.log(
-  url.format(urlObject)
-);
+console.log(url.format(urlObject)); // concatenate url properly
