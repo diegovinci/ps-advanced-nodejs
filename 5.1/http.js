4dd255be0e1ec3ad08de8b1281b958448283da80
Diego Vinci <[omitted]>
2020-04-19 16:56:43 -0300

    5.1: add other possibilities to explore some  features of the http module.

--- 5.1/http.js
+++ 5.1/http.js
@@ -2,6 +2,8 @@ const server = require('http').createServer();
 
 server.on('request', (req, res) => {
   res.writeHead(200, { 'content-type': 'text/plain' });
+  // res.end signals to the server that all of the response headers and body have been sent
+  // res.end('Hello world\n');
   res.write('Hello world\n');
 
   setTimeout(function () {
@@ -11,6 +13,15 @@ server.on('request', (req, res) => {
   setTimeout(function () {
     res.write('Yet Another Hello world\n');
   }, 20000);
+
+  setTimeout(function () {
+    res.write('Ain\'t gonna happen\n');
+  }, 130000); // after default server timeout which is 2 minutes
 });
 
+// We can control the server timeout by using the property timeout with the number of milliseconds of inactivity before a socket is presumed to have timed out.
+// server.timeout = 1000;
+
 server.listen(8000);
+
+// For testing purposes: curl localhost:8000 or curl -i localhost:8000
