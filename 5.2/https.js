d43441d0a85b8f33163cfc15e650f1ca0c4a40dc
Diego Vinci <[omitted]>
2020-04-19 18:28:17 -0300

    5.2: add instructions to create a https server and the commands to generate a certificate and a private key.

--- 5.2/https.js
+++ 5.2/https.js
@@ -10,4 +10,10 @@ server.on('request', (req, res) => {
   res.end('Hello world\n');
 });
 
-server.listen(443);
+server.listen(443); // 443: default port for https communication
+
+// For testing purposes, "openssl" comand line tool was used in the terminal to generate a private key and a certificate. 
+// Eg: openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -nodes
+// To run the application on port 443 is required to call sudo on a debian based OS. 
+// Eg: sudo /home/{user}/.nvm/versions/node/v8.16.0/bin/node https
+// And finally, access: https://localhost:443
