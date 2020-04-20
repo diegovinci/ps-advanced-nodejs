df17cf891b480615869669f244da1aad484ca6ac
Diego Vinci <[omitted]>
2020-04-19 21:16:48 -0300

    6.3: add some notes about the available methods of console module

--- 6.3/index.js
+++ 6.3/index.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 
+// console.log() // writes to stdout
+// console.info() // alias to console.log()
+// console.warn() //alias to console.error()
+// console.error() // behave exactly like console.log() but writes to stderr
+// console.trace('here') // behave like console.error() but also prints the callstack at the point where it is placed
+// console.assert(3 === '3') // perform simple assertions
+// console.time('here'); console.timeEnd('here') // return the duration of the operations between the two commands 
+
 const out = fs.createWriteStream('./out.log');
 const err = fs.createWriteStream('./err.log');
 
 const console2 = new console.Console(out, err);
 
+// writes to ./out.log or ./err.log files every 5 seconds
 setInterval(function () {
   console2.log(new Date());
   console2.error(new Error('Whoops'));
