54da8c00da6bf43e65c7841c40056cd40c62055f
Diego Vinci <[omitted]>
2020-04-19 22:08:02 -0300

    7.1: add notes about the comparisson made between the usage of readFile and creadteReadStream methods

--- 7.1/server.js
+++ 7.1/server.js
@@ -2,14 +2,24 @@ const fs = require('fs');
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
+  // In the first example, the big.file has 1M lines and more than 400mb:
+  // fs.readFile copied the entire file to the memory making node process to have more than 400mb in a few seconds.
+  // In the second example, the big.file has 5m lines and 2.2GB:
+  // fs.readFile returned an exception saying that the file size is greater than buffer limit.
+
   // fs.readFile('./big.file', (err, data) => {
   //   if (err) throw err;
-  //
   //   res.end(data);
   // });
+
+  // fs.createReadStream read the file as a stream of data, one chunck at a time, optimizing the memory allocation. 
+  // In the first example, node process consumed progressively 40mb of memory to read the ./big.file.
+  // In the second example, node process consumed almost the same amount of memory (45mb) to read the file despite this one being 5x bigger.
 
   const src = fs.createReadStream('./big.file');
   src.pipe(res);
 });
 
 server.listen(8000);
+
+// curl localhost:8000
