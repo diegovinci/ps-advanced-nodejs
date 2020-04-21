f33c0aea85045b93441405d9b0ba1a329c9f3c19
Diego Vinci <[omitted]>
2020-04-21 16:27:57 -0300

    8.3: add notes about child process

--- 8.3/exec.js
+++ 8.3/exec.js
@@ -1,5 +1,8 @@
 const { exec } = require('child_process');
 
+// By default, the spawn method does not create a shell to execute the command we passed into it, making it slightly more efficient than the exec method, which does create a shell. 
+// The exec method has one other major difference. It buffers the command's generated output and pass the whole value to a callback function.
+// Exec is a good choice if you need to use the shell syntax and the data returned from the command is not big, because exec will buffer the whole data before it returns it. 
 exec('find . -type f | wc -l', (err, stdout, stderr) => {
   if (err) {
     console.error(`exec error: ${err}`);
--- 8.3/spawn.js
+++ 8.3/spawn.js
@@ -1,5 +1,7 @@
 const { spawn } = require('child_process');
 
+// The spawn function is a much better choice than exec when the data returned from the command is big, because that data will be streamed with the standard I/O object and we can actually make the child process inherit the standard io objects of its parents if we want to. 
+
 // Shell mode
 // const child = spawn('find . -type f', {
 //   stdio: 'inherit',
