dc2ff8b828393346cdd0301d74b2babadb633fb1
Diego Vinci <[omitted]>
2020-04-21 17:18:33 -0300

    5.6: add test results about the cluster module implementation

--- 8.6/cluster.js
+++ 8.6/cluster.js
@@ -5,9 +5,13 @@ if (cluster.isMaster) {
   const cpus = os.cpus().length;
 
   console.log(`Forking for ${cpus} CPUs`);
-  for (let i = 0; i<cpus; i++) {
+  for (let i = 0; i < cpus; i++) {
     cluster.fork();
   }
 } else {
   require('./server');
 }
+
+// Each process has its own eventloop and memory space
+// ab -c200 -t10 http://localhost:8080/
+// Requests per second:    464.68 [#/sec]
--- 8.6/server.js
+++ 8.6/server.js
@@ -2,8 +2,11 @@ const http = require('http');
 const pid = process.pid;
 
 http.createServer((req, res) => {
-  for (let i=0; i<1e7; i++); // simulate CPU work
-  res.end(`Handled by process ${pid}`);
+  for (let i = 0; i < 1e7; i++); // simulate CPU work
+  res.end(`Handled by process ${pid}`); // indicate which intance of the application is actually handling the request
 }).listen(8080, () => {
   console.log(`Started process ${pid}`);
 });
+
+// ab -c200 -t10 http://localhost:8080/
+// Requests per second:    118.38 [#/sec]
